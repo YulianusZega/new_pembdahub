02c12ff41eb4679753d19ee18dd5885362a753f7
Yulianus <yulzega@gmail.com>
2026-07-11 20:00:17 +0700

    SECURITY FIX: Remove compromised polyfill.io script to prevent auth traps and malicious redirects

--- resources/views/forum/show.blade.php
+++ resources/views/forum/show.blade.php
@@ -16,7 +16,6 @@
   }
 </script>
 <!-- MathJax for math equations -->
-<script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
 <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
 <style>
     .forum-hdr { font-family: 'Space Grotesk', sans-serif; }

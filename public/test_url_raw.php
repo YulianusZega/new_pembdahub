5ed59b831269d7c5e49333212ec706cf3514580a
Yulianus <yulzega@gmail.com>
2026-07-11 20:37:17 +0700

    Cleanup temporary diagnostic files

--- public/test_url_raw.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo "Testing raw strings:\n";
-echo "Str 1: https://perguruanpembda.com/forum/poll/1/test\n";
-echo "Str 2: https://perguruanpembda.com/forum/poll/1/vote\n";
-echo "Str 3: https://perguruanpembda.com/forum/reply/1/react\n";
-echo "Str 4: https://perguruanpembda.com/forum/1/like\n";
-echo "Str 5: ALL PRINTED OK\n";

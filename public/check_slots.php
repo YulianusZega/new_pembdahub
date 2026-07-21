5b422d85e03f426ac5e3868b8a7f180f68f4f78a
Yulianus <yulzega@gmail.com>
2026-07-22 05:28:45 +0700

    chore: all slots

--- public/check_slots.php
+++ public/check_slots.php
@@ -4,9 +4,9 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
 $kernel->bootstrap();
 header('Content-Type: text/plain');
-$ts = App\Models\TimeSlot::where('school_id', 3)->where('academic_year_id', 5)
-    ->select('day_of_week', 'slot_name')
+$ts = App\Models\TimeSlot::where('school_id', 3)
+    ->select('academic_year_id', 'day_of_week', 'slot_name')
     ->get()
     ->groupBy('day_of_week')
-    ->map(function($g) { return $g->pluck('slot_name'); });
+    ->map(function($g) { return $g->pluck('slot_name', 'academic_year_id'); });
 echo json_encode($ts, JSON_PRETTY_PRINT);

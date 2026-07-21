564abffeff214b3ab139e9e098411a055727d5b5
Yulianus <yulzega@gmail.com>
2026-07-22 00:11:42 +0700

    chore: add show_table

---
+++ public/show_table.php
@@ -0,0 +1,13 @@
+<?php
+require __DIR__.'/../vendor/autoload.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$kernel->handle(Illuminate\Http\Request::capture());
+if (php_sapi_name() !== 'cli' && request('secret') !== 'pembda99') die('Unauthorized');
+
+use Illuminate\Support\Facades\DB;
+
+$result = DB::select('SHOW CREATE TABLE teaching_assignments');
+echo "<pre>";
+print_r($result);
+echo "</pre>";

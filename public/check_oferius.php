65bb3c6491e2663034df53c5fd63b10f6b2fd6d8
Yulianus <yulzega@gmail.com>
2026-07-22 07:06:28 +0700

    update check script

--- public/check_oferius.php
+++ public/check_oferius.php
@@ -6,17 +6,9 @@ $kernel->handle(Illuminate\Http\Request::capture());
 
 if (request('secret') !== 'pembda99') die('Unauthorized');
 
-use App\Models\TimeSlot;
-use App\Models\Schedule;
 use Illuminate\Support\Facades\DB;
 
 echo "<pre>";
-$schedules = DB::select("SELECT * FROM schedules WHERE teaching_assignment_id = 6177");
-foreach ($schedules as $s) {
-    echo "Schedule ID: {$s->id}, TS_ID: {$s->time_slot_id}\n";
-    $ts = DB::select("SELECT * FROM time_slots WHERE id = ?", [$s->time_slot_id]);
-    if (!empty($ts)) {
-        echo "  -> Period: {$ts[0]->period_number}, Time: {$ts[0]->start_time} - {$ts[0]->end_time}\n";
-    }
-}
+$count = DB::table('time_slots')->where('school_id', 3)->where('day_of_week', 'tuesday')->where('is_teaching_slot', 1)->count();
+echo "Number of teaching slots on Tuesday for SMK: " . $count . "\n";
 echo "</pre>";

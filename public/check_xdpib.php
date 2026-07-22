9ab8186bd0eac21ab703f8c7da02256edcacc901
Yulianus <yulzega@gmail.com>
2026-07-22 12:55:13 +0700

    fix path check script

--- public/check_xdpib.php
+++ public/check_xdpib.php
@@ -1,6 +1,6 @@
 <?php
-require __DIR__.'/vendor/autoload.php';
-$app = require_once __DIR__.'/bootstrap/app.php';
+require __DIR__.'/../vendor/autoload.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 $kernel->handle(Illuminate\Http\Request::capture());
 

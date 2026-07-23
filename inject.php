a75a55690de20c251a80d9165b3ae38ad36c715b
Yulianus <yulzega@gmail.com>
2026-07-23 07:49:47 +0700

    chore: remove temp files

--- inject.php
+++
@@ -1,6 +0,0 @@
-<?php
-$file = 'resources/views/admin/performance_contracts/show.blade.php';
-$lines = file($file);
-$stepper = file_get_contents('progress_stepper.txt');
-array_splice($lines, 28, 0, $stepper . "\n");
-file_put_contents($file, implode('', $lines));
